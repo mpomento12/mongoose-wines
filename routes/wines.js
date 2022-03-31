4f56ef7308ecbafc5a7383541456c664e5d3fa13
mpomento <mpomento@gmail.com>
2022-03-31 11:44:09 -0700

    Added Authorization

--- routes/wines.js
+++ routes/wines.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const winesCtrl = require('../controllers/wines');
+const isLoggedIn = require('../config/auth');
+
+
 
 // GET /movies/new
 router.get('/', winesCtrl.index)
-router.get('/new', winesCtrl.new);
+router.get('/new', isLoggedIn, winesCtrl.new);
 router.get('/:id', winesCtrl.show);
-router.post('/', winesCtrl.create);
+router.post('/', isLoggedIn, winesCtrl.create);
 
 module.exports = router;
 
