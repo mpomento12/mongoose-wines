f54ee69689869ef2802ff3c89e51f348b431542c
mpomento <mpomento@gmail.com>
2022-03-31 13:00:12 -0700

    this is finished

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const reviewsCtrl = require('../controllers/reviews');
+const isLoggedIn = require('../config/auth');
 
-router.get('/reviews/:id/edit', reviewsCtrl.edit);
-router.put('/reviews/:id', reviewsCtrl.update);
-router.post('/wines/:id/reviews', reviewsCtrl.create);
+router.get('/reviews/:id/edit', isLoggedIn, reviewsCtrl.edit);
+router.put('/reviews/:id', isLoggedIn, reviewsCtrl.update);
+router.post('/wines/:id/reviews', isLoggedIn, reviewsCtrl.create);
 router.delete('/reviews/:id', reviewsCtrl.delete)
 
 
